095cdf57e72ebc392d412d416b695d92c05c041b
caykennedy <111911400+caykennedy@users.noreply.github.com>
2022-10-27 16:31:49 -0600

    there

--- assignments/labs/la10/assignment10.js
+++ assignments/labs/la10/assignment10.js
@@ -1,30 +1,79 @@
+/*
 fetch('./company.json')
 .then(response => response.text())
 .then(text => humanResources(text))
+*/
 
-
-function humanResources(inString)
+//json
+const json = 
 {
-    const hr = JSON.parse(inString);
-    //problem 4
-    let totalSalary = 0;
-
-    for (let i = 0; i < hr[0].employees.length; i++)
-    {
-        totalSalary += hr[0].employees[i].salary;
-    }
-
-    console.log(hr);
-
-    //problem 5
-    for (let i = 0; i < hr[0].employees.length; i++)
-    {
-        if (hr[0].employees[i].raiseEligible === "true")
+    "companyName" : "Tech Stars",
+    "website" : "www.techstars.site",
+    "employees" :
+    [
         {
-            hr[0].employees[i].salary += employees[i].salary*0.1;
-            hr[0].employees[i].raiseEligible = "false";
+            "firstName" : "Sam",
+            "depatment" : "Tech",
+            "designation" : "Manager",
+            "salary" : "40000",
+            "raiseEligible" : "true",
+            "wfh" : "true"
+        },
+        {
+            "firstName" : "Mary",
+            "depatment" : "Finance",
+            "designation" : "Trainee",
+            "salary" : "18500",
+            "raiseEligible" : "true",
+            "wfh" : "false"
+        },
+        {
+            "firstName" : "Bill",
+            "depatment" : "HR",
+            "designation" : "Executive",
+            "salary" : "21200",
+            "raiseEligible" : "false",
+            "wfh" : "false"
         }
-    }
-    
-    console.log(hr);
+    ]
 }
+
+const newEmployee = 
+{
+    "firstName" : "Anna",
+    "depatment" : "Tech",
+    "designation" : "Executive",
+    "salary" : "25600",
+    "raiseEligible" : "false",
+    "wfh" : "true"
+}
+
+
+console.log(json);
+
+json["employees"].push(newEmployee);
+
+console.log(json);
+
+
+//problem 4
+let totalSalary = 0;
+
+for (let i = 0; i < json["employees"].length; i++)
+{
+    totalSalary += Number(json["employees"][i].salary);
+}
+
+console.log('The total salary is: ' + totalSalary);
+
+//problem 5
+for (let i = 0; i < json["employees"].length; i++)
+{
+    if (json["employees"][i].raiseEligible === "true")
+    {
+        json["employees"][i].salary = Number(json["employees"][i].salary) + Number(json["employees"][i].salary)*0.1;
+        json["employees"][i].raiseEligible = "false";
+    }
+}
+
+console.log(json);
