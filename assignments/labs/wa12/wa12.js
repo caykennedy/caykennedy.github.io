280aa97825f0662f067cea8aecac41e8bb2d4a23
caykennedy <111911400+caykennedy@users.noreply.github.com>
2022-11-08 12:58:05 -0700

    haha good luck

--- assignments/labs/wa12/wa12.js
+++ assignments/labs/wa12/wa12.js
@@ -1,9 +1,38 @@
-const button = document.querySelector('.button');
+const minusbutton = document.querySelector('.subtract-button').addEventListener('click', subtract);
+const plusbutton = document.querySelector('.add-button').addEventListener('click', add);
+const submitbutton = document.querySelector('.submit-button').addEventListener('click', submit);
+const resetbutton = document.querySelector('.reset-button').addEventListener('click', reset);
+
 const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
 
-button.addEventListener('click', updateOutput);
+function subtract()
+{
+    if (outputInt > 0)
+    {
+        outputInt -= 1;
+        output.textContent = outputInt;
+    }
 
-function updateOutput() {
-    output.textContent = phone_content.value;
+}
+
+function add()
+{
+    if (outputInt < 9999999999) 
+    {
+        outputInt +=1;
+        output.textContent = outputInt;
+    }
+}
+
+function reset()
+{
+    outputInt = 0;
+    output.textContent = outputInt;
+}
+
+function submit()
+{
+    alert(output.textContent);
 }
