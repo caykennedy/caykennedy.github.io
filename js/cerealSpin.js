a15ac3f203fbc7d28b491bfe906d2f57f079e2f4
caykennedy <111911400+caykennedy@users.noreply.github.com>
2022-11-29 23:14:16 -0700

    spain without the A

---
+++ js/cerealSpin.js
@@ -0,0 +1,35 @@
+let loaderContainer = document.querySelector('.loadingContainer');
+let cereal = document.querySelector('#cereal');
+
+const spinTiming = {
+    duration: 750,
+    iterations: 1,
+    fill: 'forwards'
+}
+
+const cerealSpinIn = [
+    { transform: 'rotate(0) scale(0)' },
+    { transform: 'rotate(360deg) scale(1)' }
+];
+
+const cerealSpinOut = [
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
+];
+
+
+const fadeTiming = {
+    duration: 500,
+    iterations: 1,
+    fill: 'forwards'
+}
+
+const fadeLoader = [
+    {opacity: 100},
+    {opacity: 0}
+]
+
+cereal.animate(cerealSpinIn,spinTiming).finished
+.then(response=>cereal.animate(cerealSpinOut,spinTiming).finished)
+.then(response=>loaderContainer.animate(fadeLoader, fadeTiming).finished)
+.then(response => loaderContainer.parentElement.removeChild(loaderContainer));
