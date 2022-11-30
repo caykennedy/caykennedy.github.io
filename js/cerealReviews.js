d222da588d0e5d0749dbdee3cdaffba6d56478b7
caykennedy <111911400+caykennedy@users.noreply.github.com>
2022-11-30 16:33:46 -0700

    update update

---
+++ js/cerealReviews.js
@@ -0,0 +1,203 @@
+const json =
+[
+    {
+        "Name" : "Phredeigh",
+        "Profile" : "Phredeigh is the founder and owner of Freddie's Secret Cereal Lounge. He has a degree in cereology, and has dedicated his life to his love of cereal.",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    },
+    {
+        "Name" : "Leon",
+        "Profile" : "",
+        "imgSrc"  : "",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    },
+    {
+        "Name" : "Steve",
+        "Profile" : "",
+        "imgSrc"  : "",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    },
+    {
+        "Name" : "Kevin",
+        "Profile" : "Kevin threatened us with a knife. He's not qualified to review cereal. Please, we're being held hostage.",
+        "imgSrc"  : "",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    },
+    {
+        "Name" : "Jimothy",
+        "Profile" : "",
+        "imgSrc"  : "",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    },
+    {
+        "Name" : "Craig",
+        "Profile" : "",
+        "imgSrc"  : "",
+        "cerealRanking" : [
+            {
+                "Number" : "1",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "2",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "3",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "4",
+                "cerealName" : "",
+                "review" : ""
+            },
+            {
+                "Number" : "5",
+                "cerealName" : "",
+                "review" : ""
+            }
+        ]
+    }
+]
+
+const phredeighButton = document.querySelector('#Phredeigh');
+const leonButton = document.querySelector('#Leon');
+const kevinButton = document.querySelector('#Kevin');
+const steveButton = document.querySelector('#Steve');
+const craigButton = document.querySelector('#Craig');
+const jimothyButton = document.querySelector('#Jimothy');
+
+let textArea = document.querySelector(reviewContainer);
