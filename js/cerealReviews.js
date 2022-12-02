cbc73b3370920265ac21dbf3a4bdeb090d52802c
caykennedy <111911400+caykennedy@users.noreply.github.com>
2022-12-01 22:56:03 -0700

    slowly but surely

--- js/cerealReviews.js
+++ js/cerealReviews.js
@@ -1,96 +1,85 @@
 const json =
 [
     {
-        "Name" : "Phredeigh",
-        "Profile" : "Phredeigh is the founder and owner of Freddie's Secret Cereal Lounge. He has a degree in cereology, and has dedicated his life to his love of cereal.",
+        "Name" : "Phredeigh Cereal-Lounge",
+        "Profile" : "Phredeigh is the founder and owner of Freddie's Secret Cereal Lounge. He has a degree in Cereology from the University of Kellogg's, and has dedicated his life to his love of cereal.",
+        "imgSrc"  : "",
+        "imgAlt" : "",
         "cerealRanking" : [
             {
-                "Number" : "1",
-                "cerealName" : "",
+                "cerealName" : "Frosted Flakes",
                 "review" : ""
             },
             {
-                "Number" : "2",
-                "cerealName" : "",
+                "cerealName" : "Honey Nut Cheerios",
                 "review" : ""
             },
             {
-                "Number" : "3",
-                "cerealName" : "",
+                "cerealName" : "Trix",
                 "review" : ""
             },
             {
-                "Number" : "4",
-                "cerealName" : "",
+                "cerealName" : "Cocoa Puffs",
                 "review" : ""
             },
             {
-                "Number" : "5",
-                "cerealName" : "",
+                "cerealName" : "Reeses Puffs",
                 "review" : ""
             }
         ]
     },
     {
-        "Name" : "Leon",
-        "Profile" : "",
-        "imgSrc"  : "",
+        "Name" : "Leon Von Leopard",
+        "Profile" : "Leon is one of Phredeigh's business partners who studied Cereology at the University of General Mills. He is quite the health nut, and is always trying to stay fit.",
+        "imgSrc"  : "./img/leon.png",
+        "imgAlt" : "Leon",
         "cerealRanking" : [
             {
-                "Number" : "1",
-                "cerealName" : "",
-                "review" : ""
+                "cerealName" : "Honey Bunches of Oats",
+                "review" : "Tasty"
             },
             {
-                "Number" : "2",
-                "cerealName" : "",
-                "review" : ""
+                "cerealName" : "Kix",
+                "review" : "Dont worry about it"
             },
             {
-                "Number" : "3",
-                "cerealName" : "",
-                "review" : ""
+                "cerealName" : "Special K With Strawberries",
+                "review" : "Fagettaboutit"
             },
             {
-                "Number" : "4",
-                "cerealName" : "",
-                "review" : ""
+                "cerealName" : "Cheerios",
+                "review" : "Gobagool"
             },
             {
-                "Number" : "5",
-                "cerealName" : "",
-                "review" : ""
+                "cerealName" : "Weetabix",
+                "review" : "Gobble Ghoul"
             }
         ]
     },
     {
-        "Name" : "Steve",
-        "Profile" : "",
-        "imgSrc"  : "",
+        "Name" : "Mailman Steve",
+        "Profile" : "Steve is our mail man, but we trust his opinions on cereal.",
+        "imgSrc"  : "./img/steve.png",
+        "imgAlt" : "Steve",
         "cerealRanking" : [
             {
-                "Number" : "1",
-                "cerealName" : "",
+                "cerealName" : "Fruity Pebbles",
                 "review" : ""
             },
             {
-                "Number" : "2",
-                "cerealName" : "",
+                "cerealName" : "Lucky Charms",
                 "review" : ""
             },
             {
-                "Number" : "3",
-                "cerealName" : "",
+                "cerealName" : "Frosted Flakes",
                 "review" : ""
             },
             {
-                "Number" : "4",
-                "cerealName" : "",
+                "cerealName" : "Frosted Mini Wheats",
                 "review" : ""
             },
             {
-                "Number" : "5",
-                "cerealName" : "",
+                "cerealName" : "Captain Crunch Berries",
                 "review" : ""
             }
         ]
@@ -98,95 +87,55 @@ const json =
     {
         "Name" : "Kevin",
         "Profile" : "Kevin threatened us with a knife. He's not qualified to review cereal. Please, we're being held hostage.",
-        "imgSrc"  : "",
+        "imgSrc"  : "./img/kevin.png",
+        "imgAlt" : "Kevin",
         "cerealRanking" : [
             {
-                "Number" : "1",
-                "cerealName" : "",
+                "cerealName" : "Captain Crunch Berries",
                 "review" : ""
             },
             {
-                "Number" : "2",
-                "cerealName" : "",
+                "cerealName" : "Apple Jacks",
                 "review" : ""
             },
             {
-                "Number" : "3",
-                "cerealName" : "",
+                "cerealName" : "Froot Loops",
                 "review" : ""
             },
             {
-                "Number" : "4",
-                "cerealName" : "",
+                "cerealName" : "Honey Smacks",
                 "review" : ""
             },
             {
-                "Number" : "5",
-                "cerealName" : "",
+                "cerealName" : "Cinnamon Toast Crunch",
                 "review" : ""
             }
         ]
     },
     {
-        "Name" : "Jimothy",
-        "Profile" : "",
-        "imgSrc"  : "",
+        "Name" : "Jumpin' Jimothy",
+        "Profile" : "Jimothy is just a little guy, but he's got big opinions! He studied with Phredeigh at the University of General Mills, and if we're being honest with you, Jimothy throws the best parties.",
+        "imgSrc"  : "./img/Jimothy.png",
+        "imgAlt" : "Jimothy",
         "cerealRanking" : [
             {
-                "Number" : "1",
-                "cerealName" : "",
+                "cerealName" : "Count Chocula",
                 "review" : ""
             },
             {
-                "Number" : "2",
-                "cerealName" : "",
+                "cerealName" : "Cocoa Puffs",
                 "review" : ""
             },
             {
-                "Number" : "3",
-                "cerealName" : "",
+                "cerealName" : "Chocolate Cheerios",
                 "review" : ""
             },
             {
-                "Number" : "4",
-                "cerealName" : "",
+                "cerealName" : "Krave",
                 "review" : ""
             },
             {
-                "Number" : "5",
-                "cerealName" : "",
-                "review" : ""
-            }
-        ]
-    },
-    {
-        "Name" : "Craig",
-        "Profile" : "",
-        "imgSrc"  : "",
-        "cerealRanking" : [
-            {
-                "Number" : "1",
-                "cerealName" : "",
-                "review" : ""
-            },
-            {
-                "Number" : "2",
-                "cerealName" : "",
-                "review" : ""
-            },
-            {
-                "Number" : "3",
-                "cerealName" : "",
-                "review" : ""
-            },
-            {
-                "Number" : "4",
-                "cerealName" : "",
-                "review" : ""
-            },
-            {
-                "Number" : "5",
-                "cerealName" : "",
+                "cerealName" : "Cocoa Pebbles",
                 "review" : ""
             }
         ]
@@ -197,7 +146,40 @@ const phredeighButton = document.querySelector('#Phredeigh');
 const leonButton = document.querySelector('#Leon');
 const kevinButton = document.querySelector('#Kevin');
 const steveButton = document.querySelector('#Steve');
-const craigButton = document.querySelector('#Craig');
 const jimothyButton = document.querySelector('#Jimothy');
 
-let textArea = document.querySelector(reviewContainer);
+let imgArea = document.querySelector('#charPortrait');
+let nameArea = document.querySelector('#charName');
+let profileArea = document.querySelector('#profile');
+let listArea = document.querySelector('#cerealRankings');
+let topFiveHeader = document.querySelector('#topFive');
+let reviewArea = document.querySelector('.reviewContainer')
+
+phredeighButton.addEventListener('click', function(){change(0)});
+leonButton.addEventListener('click', function(){change(1)});
+steveButton.addEventListener('click', function(){change(2)});
+kevinButton.addEventListener('click',function(){change(3)});
+jimothyButton.addEventListener('click',function(){change(4)});
+
+
+
+function change(num)
+{
+    imgArea.setAttribute('src',json[num]['imgSrc']);
+    imgArea.setAttribute('alt',json[num]['imgAlt']);
+    nameArea.textContent = json[num]['Name'];
+    profileArea.textContent = json[num]['Profile'];
+    topFiveHeader.textContent = json[num]['Name'] + "'s Top Five"
+
+    listArea.replaceChildren();
+
+    for (let i = 0; i < 5; i++)
+    {
+        var li = document.createElement('li');
+        li.innerText = json[num]['cerealRanking'][i]['cerealName'] + " - " + json[num]['cerealRanking'][i]['review'];
+        listArea.appendChild(li);
+    }
+
+    reviewArea.removeAttribute("style");
+
+}
